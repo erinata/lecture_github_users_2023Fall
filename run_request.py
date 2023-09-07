1e0718007de98aeb7757450d74eaf05cf15b1f17
Chungsang Tom Lam <erinata@gmail.com>
2023-09-07 12:17:29 -0400

    rate limit requests

--- run_request.py
+++ run_request.py
@@ -1,2 +1,9 @@
-print("Github Users")
+import requests
+import json
+
+access_point = "https://api.github.com"
+
+response_text = requests.get(access_point + "/rate_limit").text
+
+print(json.loads(response_text))
 
