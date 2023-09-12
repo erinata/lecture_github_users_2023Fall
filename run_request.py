33ec9e836f2f1da08ff4183343c1793b07f96ce8
Chungsang Tom Lam <erinata@gmail.com>
2023-09-12 17:00:11 -0400

    login to github

--- run_request.py
+++ run_request.py
@@ -8,12 +8,20 @@ if not os.path.exists("json_files"):
 
 access_point = "https://api.github.com"
 
-response_text = requests.get(access_point + "/rate_limit").text
+f = open("token", "r")
+token = f.read()
+f.close()
+
+github_session = requests.Session()
+github_session.auth = ("erinata", token)
+
+
+response_text = github_session.get(access_point + "/rate_limit").text
 print(json.loads(response_text))
 
 
 user_id = "erinata"
-response_text = requests.get(access_point + "/users/" + user_id).text
+response_text = github_session.get(access_point + "/users/" + user_id).text
 
 json_text = json.loads(response_text)
 
