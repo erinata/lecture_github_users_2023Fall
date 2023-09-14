deab33fec7631c39ea8f977a7680d874d6edf3fd
Chungsang Tom Lam <erinata@gmail.com>
2023-09-14 11:42:36 -0400

    loop through many ids

--- run_request.py
+++ run_request.py
@@ -1,16 +1,20 @@
 import requests
 import json
 import os
+import pandas
+import time
 
 if not os.path.exists("json_files"):
   os.mkdir("json_files")
-
 
 access_point = "https://api.github.com"
 
 f = open("token", "r")
 token = f.read()
 f.close()
+
+id_list = pandas.read_csv("seed.csv")
+id_list = id_list['ghid']
 
 github_session = requests.Session()
 github_session.auth = ("erinata", token)
@@ -19,16 +23,21 @@ github_session.auth = ("erinata", token)
 response_text = github_session.get(access_point + "/rate_limit").text
 print(json.loads(response_text))
 
+for user_id in id_list:
+  
+  # user_id = "erinata"
+  print(user_id)
+  response_text = github_session.get(access_point 
+                     + "/users/" + user_id).text
 
-user_id = "erinata"
-response_text = github_session.get(access_point + "/users/" + user_id).text
+  json_text = json.loads(response_text)
 
-json_text = json.loads(response_text)
-
-file_name = "json_files/" + user_id + ".json"
-f = open(file_name, "w")
-f.write(json.dumps(json_text))
-f.close()
+  file_name = "json_files/" + user_id + ".json"
+  f = open(file_name, "w")
+  f.write(json.dumps(json_text))
+  f.close()
+  time.sleep(1)
+  
 
 
 
