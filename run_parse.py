33b452c0bebae659f75a18a9a0f83f1bb53d6b54
Chungsang Tom Lam <erinata@gmail.com>
2023-09-19 11:56:46 -0400

    write to csv

--- run_parse.py
+++ run_parse.py
@@ -8,7 +8,7 @@ if not os.path.exists("parsed_files"):
 
 dataset = pandas.DataFrame()
 
-json_file_name = "json_files/erinata.json"
+json_file_name = "json_files/dAAAb.json"
 
 f = open(json_file_name, "r")
 json_data = json.load(f)
@@ -18,15 +18,30 @@ f.close()
 
 gh_id = json_data['login']
 gh_number_id = json_data['id']
-plan_name = json_data['plan']['name']
 updated_at = json_data['updated_at']
 followers = json_data['followers']
 
 
 print(gh_id)
 print(gh_number_id)
-print(plan_name)
 print(updated_at)
 print(followers)
+
+row = pandas.DataFrame.from_records(
+  [
+  {
+    'gh_id': gh_id,
+    'gh_number_id': gh_number_id,
+    'updated_at': updated_at,
+    'followers': followers
+  }
+  ]
+  )
+
+print(row)
+dataset = pandas.concat([dataset, row])
+
+dataset.to_csv("parsed_files/github_user_data.csv"
+                , index=False)
 
 
