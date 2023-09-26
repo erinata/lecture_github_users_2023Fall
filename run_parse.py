8cea0db807a2743c2dc4b630dbeb5b4d30129391
Chungsang Tom Lam <erinata@gmail.com>
2023-09-26 12:13:40 -0400

    followers

--- run_parse.py
+++ run_parse.py
@@ -1,6 +1,7 @@
 import json
 import pandas
 import os
+import glob
 
 if not os.path.exists("parsed_files"):
   os.mkdir("parsed_files")
@@ -8,38 +9,40 @@ if not os.path.exists("parsed_files"):
 
 dataset = pandas.DataFrame()
 
-json_file_name = "json_files/dAAAb.json"
+for json_file_name in glob.glob("json_files/*.json"):
+  # json_file_name = "json_files/dAAAb.json"
 
-f = open(json_file_name, "r")
-json_data = json.load(f)
-f.close()
+  f = open(json_file_name, "r")
+  json_data = json.load(f)
+  f.close()
 
-# print(json_data)
+  # print(json_data)
 
-gh_id = json_data['login']
-gh_number_id = json_data['id']
-updated_at = json_data['updated_at']
-followers = json_data['followers']
+  gh_id = json_data['login']
+  gh_number_id = json_data['id']
+  updated_at = json_data['updated_at']
+  followers = json_data['followers']
+  followers_url = json_data['followers_url']
 
 
-print(gh_id)
-print(gh_number_id)
-print(updated_at)
-print(followers)
+  print(gh_id)
+  print(gh_number_id)
+  print(updated_at)
+  print(followers)
+  print(followers_url)
 
-row = pandas.DataFrame.from_records(
-  [
-  {
-    'gh_id': gh_id,
-    'gh_number_id': gh_number_id,
-    'updated_at': updated_at,
-    'followers': followers
-  }
-  ]
-  )
+  row = pandas.DataFrame.from_records([{
+        'gh_id': gh_id,
+        'gh_number_id': gh_number_id,
+        'updated_at': updated_at,
+        'followers': followers,
+        'followers_url': followers_url
+      }])
 
-print(row)
-dataset = pandas.concat([dataset, row])
+  print(row)
+  dataset = pandas.concat([dataset, row])
+
+
 
 dataset.to_csv("parsed_files/github_user_data.csv"
                 , index=False)
--- run_request_followers.py
+++ run_request_followers.py
@@ -4,37 +4,36 @@ import os
 import pandas
 import time
 
-if not os.path.exists("json_files"):
-  os.mkdir("json_files")
-
+if not os.path.exists("followers_json_files"):
+  os.mkdir("followers_json_files")
+    
 access_point = "https://api.github.com"
 
 f = open("token", "r")
 token = f.read()
 f.close()
 
-id_list = pandas.read_csv("input_files/seed.csv")
-id_list = id_list['ghid']
+followers_url_list = pandas.read_csv("parsed_files/github_user_data.csv")
+followers_url_list = followers_url_list['followers_url']
 
 github_session = requests.Session()
 github_session.auth = ("erinata", token)
 
-
 response_text = github_session.get(access_point + "/rate_limit").text
 print(json.loads(response_text))
 
-for user_id in id_list:
-  file_name = "json_files/" + user_id + ".json"
+for followers_url in followers_url_list:
+  user_id = followers_url.split("/")[-2]
+  file_name = "followers_json_files/" + user_id + ".json"
   
   if os.path.exists(file_name):
     # pass
     print("File exists: ", user_id)
+
   else:
     try:
-      # user_id = "erinata"
       print(user_id)
-      response_text = github_session.get(access_point 
-                         + "/users/" + user_id).text
+      response_text = github_session.get(followers_url).text
 
       json_text = json.loads(response_text)
 
@@ -48,6 +47,13 @@ for user_id in id_list:
       
     time.sleep(5)
     
+    
+
+
+
+
+
+
 
 
 
